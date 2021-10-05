d7d29f7ff21637a7a2d4caf380e924adfd31f27c
Goutham Nannuta <69679486+gouthamNannuta@users.noreply.github.com>
2021-10-05 16:48:28 +0530

    Update highlights.js

--- js/highlights.js
+++ js/highlights.js
@@ -21,39 +21,26 @@ AFRAME.registerComponent("cursor-listener", {
             selectedItemId: id,
           });
           console.log("if condition")
-          this.el.setAttribute("material", { color: "red" });
+          this.el.setAttribute("material", { color: "#1565c0" });
         }
       });
     },
     handleMouseLeaveEvents: function () {
-      // Mouse Leave Events
-      /*this.el.addEventListener("mouseleave", () => {
-        const { selectedItemId } = this.data;
+      this.el.addEventListener("mouseleave", () => {
+        const id = this.el.getAttribute("id");
         console.log("leave")
-        if (selectedItemId) {
-          console.log("leave condition")
-          const el = document.querySelector(`#${selectedItemId}`);
-          const id = el.getAttribute("id");
-          if (id === selectedItemId) {
-            console.log("if condition leave")
-            el.setAttribute("material", { color: "#fff" });
-          }
+        const postersId = [
+            "ShinChan", "Cars", "Kick-Buttowski", "Doraemon"
+        ];
+        if (postersId.includes(id)) {
+          const postersContainer = document.querySelector("#posters-container");
+          postersContainer.setAttribute("cursor-listener", {
+            selectedItemId: id,
+          });
+          console.log("if condition")
+          this.el.setAttribute("material", { color: "white" });
         }
-      });*/
-        const id = this.el.getAttribute("id")
-        const placesId = ["ShinChan","Kick-Buttowski","Cars","Doraemon"];
-
-        if(placesId.includes(id)){
-          console.log("leave")
-            const placesContainer = document.querySelector("#posters-container")
-            placesContainer.setAttribute("cursor-listener", {
-                selectedItemId: id
-            })
-            this.el.setAttribute("material", {
-                color: "#0077CC",
-                opacity: 1
-            })
-    }}
-    },
+      })
+    }},
     
   );
